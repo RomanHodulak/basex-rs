61155d629e38e19301c35221ae4e3b0a3873b450
Roman Hodulák <hodulakr@gmail.com>
2021-09-03 11:46:39 +0200

    test(Query): Test bind, context, execute, updating, info, options and failing scenarios.

--- src/query.rs
+++ src/query.rs
@@ -88,7 +88,8 @@ impl<T> Query<T> where T: DatabaseStream {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::tests::MockStream;
+    use crate::tests::{MockStream, FailingStream};
+    use crate::ClientError;
 
     impl<T> Query<T> where T: DatabaseStream {
         pub(crate) fn into_inner(self) -> Connection<T> {
@@ -98,22 +99,225 @@ mod tests {
 
     #[test]
     fn test_query_binds_arguments() {
-        let expected_response = "test_response";
-        let stream = MockStream::new(expected_response.to_owned());
+        let stream = MockStream::new("test_response".to_owned());
+        let connection = Connection::new(stream);
 
-        let stream = {
-            let connection = Connection::new(stream);
+        let mut query = Query::new("test".to_owned(), connection);
+        let _ = query.bind("foo", Some("aaa"), Some("integer")).unwrap()
+            .bind("bar", Some("123"), None).unwrap()
+            .bind("void", None, None).unwrap();
 
-            let mut query = Query::new("test".to_owned(), connection);
-            let _ = query.bind("foo", Some("aaa"), Some("integer"));
-
-            query.into_inner().into_inner()
-        };
-
+        let stream = query.into_inner().into_inner();
         let actual_buffer = stream.to_string();
-        let expected_buffer = String::from_utf8(vec![Command::Bind as u8]).unwrap()
-            + "test\u{0}foo\u{0}aaa\u{0}integer\u{0}";
+        let expected_buffer = "\u{3}test\u{0}foo\u{0}aaa\u{0}integer\u{0}\
+            \u{3}test\u{0}bar\u{0}123\u{0}\u{0}\
+            \u{3}test\u{0}void\u{0}\u{0}\u{0}".to_owned();
 
         assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_fails_to_bind_argument_with_failing_stream() {
+        let connection = Connection::new(FailingStream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_error = query.bind("foo", None, None)
+            .err().expect("Operation must fail");
+
+        assert!(matches!(actual_error, ClientError::Io(_)));
+    }
+
+    #[test]
+    fn test_query_binds_value_to_context() {
+        let stream = MockStream::new("test_response".to_owned());
+        let connection = Connection::new(stream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let _ = query.context(Some("aaa"), Some("integer")).unwrap();
+
+        let stream = query.into_inner().into_inner();
+        let actual_buffer = stream.to_string();
+        let expected_buffer = "\u{e}test\u{0}aaa\u{0}integer\u{0}".to_owned();
+
+        assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_binds_value_to_context_with_empty_type() {
+        let stream = MockStream::new("test_response".to_owned());
+        let connection = Connection::new(stream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let _ = query.context(Some("aaa"), None).unwrap();
+
+        let stream = query.into_inner().into_inner();
+        let actual_buffer = stream.to_string();
+        let expected_buffer = "\u{e}test\u{0}aaa\u{0}\u{0}".to_owned();
+
+        assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_binds_empty_value_to_context() {
+        let stream = MockStream::new("test_response".to_owned());
+        let connection = Connection::new(stream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let _ = query.context(None, None).unwrap();
+
+        let stream = query.into_inner().into_inner();
+        let actual_buffer = stream.to_string();
+        let expected_buffer = "\u{e}test\u{0}\u{0}\u{0}".to_owned();
+
+        assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_fails_to_bind_context_with_failing_stream() {
+        let connection = Connection::new(FailingStream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_error = query.context(None, None)
+            .err().expect("Operation must fail");
+
+        assert!(matches!(actual_error, ClientError::Io(_)));
+    }
+
+    #[test]
+    fn test_query_executes() {
+        let expected_response = "test_response";
+        let stream = MockStream::new(expected_response.to_owned());
+        let connection = Connection::new(stream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_response = query.execute().unwrap();
+
+        assert_eq!(expected_response, actual_response);
+
+        let stream = query.into_inner().into_inner();
+        let actual_buffer = stream.to_string();
+        let expected_buffer = "\u{5}test\u{0}".to_owned();
+
+        assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_fails_to_execute_with_failing_stream() {
+        let connection = Connection::new(FailingStream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_error = query.execute().expect_err("Operation must fail");
+
+        assert!(matches!(actual_error, ClientError::Io(_)));
+    }
+
+    #[test]
+    fn test_query_runs_updating_command() {
+        let expected_response = "test_response";
+        let stream = MockStream::new(expected_response.to_owned());
+        let connection = Connection::new(stream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_response = query.updating().unwrap();
+
+        assert_eq!(expected_response, actual_response);
+
+        let stream = query.into_inner().into_inner();
+        let actual_buffer = stream.to_string();
+        let expected_buffer = "\u{1e}test\u{0}".to_owned();
+
+        assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_fails_to_run_updating_command_with_failing_stream() {
+        let connection = Connection::new(FailingStream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_error = query.updating().expect_err("Operation must fail");
+
+        assert!(matches!(actual_error, ClientError::Io(_)));
+    }
+
+    #[test]
+    fn test_query_runs_options_command() {
+        let expected_response = "test_response";
+        let stream = MockStream::new(expected_response.to_owned());
+        let connection = Connection::new(stream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_response = query.options().unwrap();
+
+        assert_eq!(expected_response, actual_response);
+
+        let stream = query.into_inner().into_inner();
+        let actual_buffer = stream.to_string();
+        let expected_buffer = "\u{7}test\u{0}".to_owned();
+
+        assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_fails_to_run_options_command_with_failing_stream() {
+        let connection = Connection::new(FailingStream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_error = query.options().expect_err("Operation must fail");
+
+        assert!(matches!(actual_error, ClientError::Io(_)));
+    }
+
+    #[test]
+    fn test_query_runs_info_command() {
+        let expected_response = "test_response";
+        let stream = MockStream::new(expected_response.to_owned());
+        let connection = Connection::new(stream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_response = query.info().unwrap();
+
+        assert_eq!(expected_response, actual_response);
+
+        let stream = query.into_inner().into_inner();
+        let actual_buffer = stream.to_string();
+        let expected_buffer = "\u{6}test\u{0}".to_owned();
+
+        assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_fails_to_run_info_command_with_failing_stream() {
+        let connection = Connection::new(FailingStream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_error = query.info().expect_err("Operation must fail");
+
+        assert!(matches!(actual_error, ClientError::Io(_)));
+    }
+
+    #[test]
+    fn test_query_closes() {
+        let expected_response = "test_response";
+        let stream = MockStream::new(expected_response.to_owned());
+        let connection = Connection::new(stream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let _ = query.close().unwrap();
+
+        let stream = query.into_inner().into_inner();
+        let actual_buffer = stream.to_string();
+        let expected_buffer = "\u{2}test\u{0}".to_owned();
+
+        assert_eq!(expected_buffer, actual_buffer);
+    }
+
+    #[test]
+    fn test_query_fails_to_close_with_failing_stream() {
+        let connection = Connection::new(FailingStream);
+
+        let mut query = Query::new("test".to_owned(), connection);
+        let actual_error = query.close().err().expect("Operation must fail");
+
+        assert!(matches!(actual_error, ClientError::Io(_)));
     }
 }
