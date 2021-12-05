a7cf5d1434136f5b970e43016b00a72a497449f0
Roman Hodulák <hodulakr@gmail.com>
2021-12-05 15:33:25 +0100

    test(`query_with_undeclared_variable_fails`): Use if let instead of match statement.

--- tests/query_with_undeclared_variable_fails.rs
+++ tests/query_with_undeclared_variable_fails.rs
@@ -9,16 +9,15 @@ fn test_query_with_undeclared_variable_fails() -> Result<(), ClientError> {
 
     let query = client.query(&mut "$x".as_bytes())?;
     let actual_error = query.execute()?.close().err().unwrap();
-    match &actual_error {
-        ClientError::QueryFailed(q) => {
-            assert_eq!("Stopped at ., 1/1:\n[XPST0008] Undeclared variable: $x.", q.raw());
-            assert_eq!("Undeclared variable: $x.", q.message());
-            assert_eq!(1, q.line());
-            assert_eq!(1, q.position());
-            assert_eq!(".", q.file());
-            assert_eq!("XPST0008", q.code());
-        },
-        _ => assert!(false),
-    };
+    assert!(matches!(actual_error, ClientError::QueryFailed(_)));
+
+    if let ClientError::QueryFailed(q) = actual_error {
+        assert_eq!("Stopped at ., 1/1:\n[XPST0008] Undeclared variable: $x.", q.raw());
+        assert_eq!("Undeclared variable: $x.", q.message());
+        assert_eq!(1, q.line());
+        assert_eq!(1, q.position());
+        assert_eq!(".", q.file());
+        assert_eq!("XPST0008", q.code());
+    }
     Ok(())
 }
